83cff47c7be54f5fe719b8ad54169360f7c40163
vinixztx <vinixztx7@gmail.com>
2024-09-09 09:30:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); 
+}
+
+function plusSlides(n) {
+    slideIndex += n - 1;
+    if (slideIndex < 1) { slideIndex = slides.length; }
+    showSlides();
+}
